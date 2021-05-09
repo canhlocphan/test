ea2319c8ec1cae781eef318c82008ef95a3c9b68
canhlocphan <canhloc.phan@m.citynow.vn>
2021-05-09 22:57:21 +0700

    Fix

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -5,7 +5,8 @@ import "./styles.scss";
 const About = () =>
     (
     <div className="about-container">
-        <h1 className="about">About</h1>
+        <h1 className="about">About Testing Pull Request</h1>
+        <h1 className="about">About Testing Pull Request</h1>
     </div>
     )
 export default About;
